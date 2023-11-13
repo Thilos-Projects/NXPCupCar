f693b9ea3d53cfcf4ebb98f9157ca7263c77c9e3
Thilos-Projects <50086979+Thilos-Projects@users.noreply.github.com>
2023-11-13 15:19:20 +0100

    Mohtor Dreht sich

--- software/ProjectFastTrack/source/main.cpp
+++ software/ProjectFastTrack/source/main.cpp
@@ -20,7 +20,7 @@ extern "C"
 //#include "Modules/mDac.h"
 //#include "Modules/mAccelMagneto.h"
 //#include "Modules/mGyro.h"
-//#include "Modules/mTimer.h"
+#include "Modules/mTimer.h"
 #include "Modules/mCpu.h"		//mCpu_Setup
 //#include "Modules/mSwitch.h"
 #include "Modules/mLeds.h"
@@ -36,24 +36,47 @@ extern "C"
 
 int main(){
 	printf("Hello Car\n");
-
-	printf("Tshöööööööö\n");
 	mCpu_Setup();
 	mLeds_Setup();
 
+	mTimer_Setup();
+	mTimer_Open();
+
 	mLeds_Write(kMaskLed1,kLedOn);
+
+	mTimer_EnableHBridge();
+
+	mTimer_SetMotorDuty(0.4f, 0.4f);
+
+	//bool up = true;
+	//float test = 0;
 
 	for(UInt32 i = 0; true; i++){
 
+		if(mTimer_GetFaultMoteurLeft())
+			printf("motor left fault");
+		if(mTimer_GetFaultMoteurRight())
+			printf("motor right fault");
 		mLeds_Write(kMaskLed1,kLedOff);
-		for(UInt32 j = 0; j < 10000000; j++);
+		for(UInt32 j = 0; j < 2500000; j++);
 		mLeds_Write(kMaskLed1,kLedOn);
-		for(UInt32 j = 0; j < 10000000; j++);
+		for(UInt32 j = 0; j < 2500000; j++);
+
+		//if(up)
+			//test+=0.1f;
+		//else
+			//test-=0.1f;
+
+		//if(up && test > 0.9f)
+		//	up = false;
+		//if(!up && test < -0.9f)
+		//	up = true;
+
+		//mTimer_SetMotorDuty(test, 3/8.0f);
+
 	}
 
-
-
-
+	printf("Tshöööööööö\n");
 
 	return 0;
 }
