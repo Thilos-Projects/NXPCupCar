b37e474256febcd0883f90c4b9a377fdbcaa988d
Tom <77toms@gmail.com>
2023-11-16 17:24:50 +0100

    Zwischenstand Pixy Integration (Video API (RGB) + Line API)

--- software/ProjectFastTrack/source/main.cpp
+++ software/ProjectFastTrack/source/main.cpp
@@ -25,14 +25,22 @@ extern "C"
 //#include "Modules/mSwitch.h"
 #include "Modules/mLeds.h"
 //#include "Modules/mAd.h"
-//#include "Modules/mDelay.h"
+#include "Modules/mDelay.h" //Pixy Änderungen: mDelay.h wird für das Auslesen der Pixy verwändert!!!
 //#include "Modules/mRS232.h"
 //#include "Modules/mVL6180x.h"
 
 //#include "Applications/gInput.h"
 //#include "Applications/gCompute.h"
 //#include "Applications/gOutput.h"
+
 }
+
+/*----------------------------------*/
+/* Pixy 2  Notwendiges um Pixy auszulesen*/
+#include "Pixy/Pixy2SPI_SS.h"
+#define	DELTA_T 34
+#define K_MAIN_INTERVAL (DELTA_T / kPit1Period)
+/*----------------------------------*/
 
 int main(){
 	printf("Hello Car\n");
@@ -47,6 +55,59 @@ int main(){
 	mTimer_EnableHBridge();
 
 	mTimer_SetMotorDuty(0.4f, 0.4f);
+
+
+
+
+	//----------------------------------------------
+	static Int16 sDelay;
+	sDelay = mDelay_GetDelay(kPit1, K_MAIN_INTERVAL);
+	//PRINTF("Hello World\n");
+	printf("HellO World: %ld %d\n",clock(),CLOCKS_PER_SEC);
+
+	printf("MyPixyTest\n");
+
+    Pixy2SPI_SS pixy;
+    pixy.init();
+    pixy.getVersion();
+    pixy.version->print();
+    printf("HellO World: %ld\n",clock());
+    pixy.setLED(0, 255, 0);
+    //pixy.setLamp(1, 1);
+    pixy.changeProg("video");
+
+	/* Tom Ergänzungen: Pixy RGB ausprobieren */
+
+
+	uint8_t rot, gruen, blau;
+	uint8_t ergebnis;
+
+	ergebnis = pixy.video.getRGB(250, 260, &rot, &gruen, &blau, false);
+
+	for(;;) {
+		ergebnis = pixy.video.getRGB(250, 260, &rot, &gruen, &blau, false);
+		//printf("Ergebnis der getRGB Methode: %d\n",ergebnis);
+		printf("Rot Wert: %d, Gruen Wert: %d, Blau Wert: %d\n",rot, gruen, blau);
+	}
+
+
+
+	/* Test: Pixy Line Traking API */
+	/*
+	pixy.changeProg("line");
+	uint8_t ergebnis;
+
+	for(;;) {
+		ergebnis = pixy.line.getMainFeatures();
+		printf("Ergebnis: %d\n",ergebnis);
+		pixy.line.vectors->print();
+	}
+	*/
+	printf("End of Pixy Test\n");
+
+	//----------------------------------------------
+
+
 
 	//bool up = true;
 	//float test = 0;
